dc299c79492d5c948c48f060b891906e86ea9dc1
Andrey Evseev <a-evseev@yandex.ru>
2022-07-25 20:49:17 +0300

    проверка задания 4

--- Task5_4.py
+++ Task5_4.py
@@ -1,37 +1,39 @@
 # Задание 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.  
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
 
-def cleared():
+import Func_data_flow_generator as dfg
+import Func_read_files as rf
+import Func_rle_encoding as rle_enc
+import Func_writing_file as wf
+import Func_rle_decoding as dec
+
+def cleared(comprssed_file, recovered_file):
     if int(input('Для очистки рабочих файлов введите 0.\n'
         'Для сохранения - другое число: ')) == 0:
         cleared_data = ''
         cleared_file = comprssed_file
         wf.writing_file(cleared_data, cleared_file)
-        cleared_file = comprssed_file
+        cleared_file = recovered_file
         wf.writing_file(cleared_data, recovered_file)
 
-cleared()
 work_file = 'files/f_data5_4_source.txt'
+comprssed_file = 'files/f_data5_4_compressed.txt'
+recovered_file = 'files/f_data5_4_recovered.txt'
+cleared(comprssed_file, recovered_file)
+
 way = input('Если Вы хотите сгенерировать новый поток данных, введите 1,\n'
     'если нет - введите любое другое число: ')
 if int(way) == 1:
-    import Func_data_flow_generator as dfg
     dfg.data_flow_generator(work_file)
 
-import Func_read_files as rf
 incoming_stream = rf.read_file(work_file)
 
-import Func_rle_encoding as rle_enc
 compressed_data = rle_enc.rle_encoding(incoming_stream)
 
-comprssed_file = 'files/f_data5_4_compressed.txt'
-import Func_writing_file as wf
 wf.writing_file(compressed_data, comprssed_file)
 
-import Func_rle_decoding as dec
 restored_data = dec.rle_decoding(compressed_data)
 
-recovered_file = 'files/f_data5_4_recovered.txt'
 wf.writing_file(restored_data, recovered_file)
 
-cleared()
+cleared(comprssed_file, recovered_file)
